b60b561571e99fe997e16ce94249f6f749de1d71
percival013 <112486687+percival013@users.noreply.github.com>
2024-12-09 22:08:01 +0800

    Add project files.

---
+++ MovieCollectionTrackerApp/MovieTracker.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace MovieCollectionTrackerApp
+{
+    partial class MovieTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MovieTracker));
+            this.txtReleaseYear = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtDirector = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.dgvMovies = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMovieCount = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMovies)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtReleaseYear
+            // 
+            this.txtReleaseYear.BackColor = System.Drawing.Color.Honeydew;
+            this.txtReleaseYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReleaseYear.Location = new System.Drawing.Point(18, 397);
+            this.txtReleaseYear.Name = "txtReleaseYear";
+            this.txtReleaseYear.Size = new System.Drawing.Size(308, 29);
+            this.txtReleaseYear.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtDirector
+            // 
+            this.txtDirector.BackColor = System.Drawing.Color.Honeydew;
+            this.txtDirector.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDirector.Location = new System.Drawing.Point(18, 326);
+            this.txtDirector.Name = "txtDirector";
+            this.txtDirector.Size = new System.Drawing.Size(308, 29);
+            this.txtDirector.TabIndex = 2;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.BackColor = System.Drawing.Color.Honeydew;
+            this.txtGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGenre.Location = new System.Drawing.Point(18, 256);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(308, 29);
+            this.txtGenre.TabIndex = 3;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.Honeydew;
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(18, 189);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(308, 29);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnAdd.Location = new System.Drawing.Point(18, 445);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(155, 44);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add Movie";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.btnUpdate.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnUpdate.Location = new System.Drawing.Point(181, 445);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(145, 44);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update Movie";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnDelete.Location = new System.Drawing.Point(18, 499);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(156, 44);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete Movie";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.btnViewAll.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnViewAll.Location = new System.Drawing.Point(181, 499);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(145, 44);
+            this.btnViewAll.TabIndex = 8;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // dgvMovies
+            // 
+            this.dgvMovies.BackgroundColor = System.Drawing.Color.Honeydew;
+            this.dgvMovies.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvMovies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMovies.GridColor = System.Drawing.Color.White;
+            this.dgvMovies.Location = new System.Drawing.Point(352, 66);
+            this.dgvMovies.MultiSelect = false;
+            this.dgvMovies.Name = "dgvMovies";
+            this.dgvMovies.RowHeadersWidth = 51;
+            this.dgvMovies.RowTemplate.Height = 24;
+            this.dgvMovies.Size = new System.Drawing.Size(728, 480);
+            this.dgvMovies.TabIndex = 9;
+            this.dgvMovies.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMovies_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Release Year:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(23, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Genre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(23, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Directed by:";
+            // 
+            // lblMovieCount
+            // 
+            this.lblMovieCount.AutoSize = true;
+            this.lblMovieCount.Font = new System.Drawing.Font("Webdings", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovieCount.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblMovieCount.Location = new System.Drawing.Point(129, 64);
+            this.lblMovieCount.Name = "lblMovieCount";
+            this.lblMovieCount.Size = new System.Drawing.Size(89, 97);
+            this.lblMovieCount.TabIndex = 15;
+            this.lblMovieCount.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(45, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(256, 37);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Current Movies:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.Color.Honeydew;
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.txtSearch.Location = new System.Drawing.Point(442, 26);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(541, 29);
+            this.txtSearch.TabIndex = 17;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnSearch.Location = new System.Drawing.Point(989, 27);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(91, 28);
+            this.btnSearch.TabIndex = 18;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(350, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Search:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnClear.Location = new System.Drawing.Point(251, 160);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 20;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MovieTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1119, 574);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMovieCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvMovies);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtDirector);
+            this.Controls.Add(this.txtReleaseYear);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MovieTracker";
+            this.Text = "Movie Collection Tracker";
+            this.Load += new System.EventHandler(this.MovieTracker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMovies)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReleaseYear;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtDirector;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.DataGridView dgvMovies;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMovieCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MovieCollectionTrackerApp/MovieTracker.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MovieCollectionTrackerApp
+{
+    public partial class MovieTracker : Form
+    {
+        private Database db = new Database();
+        public MovieTracker()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string genre = txtGenre.Text;
+            int releaseYear = int.Parse(txtReleaseYear.Text);
+            string director = txtDirector.Text;
+
+            string query = $"INSERT INTO Movies (Title, Genre, ReleaseYear, Director)" +
+                           $" VALUES ('{title}', '{genre}', {releaseYear}, '{director}')";
+
+            db.ExecuteNonQuery(query);
+
+            MessageBox.Show("Movie added!");
+            LoadMovies();
+        }
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            LoadMovies();
+        }
+        private void LoadMovies()
+        {
+            string query = "SELECT * FROM Movies";
+            DataTable movies = db.ExecuteQuery(query);
+            dgvMovies.DataSource = movies;
+
+            MovieCount();
+        }       
+        private void MovieCount()
+        {
+            string query = "SELECT * FROM Movies";
+            DataTable movies = db.ExecuteQuery(query);
+            int movieCount = movies.Rows.Count;
+            lblMovieCount.Text = movieCount.ToString();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvMovies.SelectedRows.Count > 0)
+            {
+                int movieId = (int)dgvMovies.SelectedRows[0].Cells["MovieID"].Value;
+                string title = txtTitle.Text;
+                string genre = txtGenre.Text;
+                int releaseYear = Convert.ToInt32(txtReleaseYear.Text);
+                string director = txtDirector.Text;
+
+                string query = $"UPDATE Movies SET Title='{title}', Genre='{genre}', ReleaseYear={releaseYear}, Director='{director}' WHERE MovieId={movieId}";
+
+                db.ExecuteNonQuery(query);
+
+                MessageBox.Show("Movie Updated!");
+                LoadMovies();
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvMovies.SelectedRows.Count > 0)
+            {
+                int movieId = (int)dgvMovies.SelectedRows[0].Cells["MovieID"].Value;
+
+                var confirm = MessageBox.Show("Are you sure to delete this movie?", "Confirm Delete", MessageBoxButtons.YesNo);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    string query = $"DELETE FROM Movies WHERE MovieID = {movieId}";
+
+                    db.ExecuteNonQuery(query);
+
+                    MessageBox.Show("Movie deleted!");
+                    LoadMovies();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a movie to delete");
+                }
+            }
+        }
+        private void MovieTracker_Load(object sender, EventArgs e)
+        {
+            LoadMovies();
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text;
+
+            string query = $"SELECT * FROM Movies " +
+                           $"WHERE Title LIKE '%{keyword}%'" +
+                           $" OR Genre LIKE '%{keyword}%'" +
+                           $" OR ReleaseYear LIKE '%{keyword}%'" +
+                           $" OR Director LIKE '%{keyword}%'"; 
+            try
+            {
+                DataTable searchResults = db.ExecuteQuery(query);
+                dgvMovies.DataSource = searchResults;
+
+                txtSearch.Text = "";
+                MovieCount();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void dgvMovies_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvMovies.Rows[e.RowIndex];
+
+                txtTitle.Text = row.Cells["Title"].Value.ToString();
+                txtGenre.Text = row.Cells["Genre"].Value.ToString();
+                txtReleaseYear.Text = row.Cells["ReleaseYear"].Value.ToString();
+                txtDirector.Text = row.Cells["Director"].Value.ToString();
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTitle.Text = "";
+            txtGenre.Text = "";
+            txtReleaseYear.Text = "";
+            txtDirector.Text = "";
+        }
+    }
+    public class Database
+    {
+        private string connString = "server=localhost;database=MovieDB;uid=root;pwd=BKjonpCFvhw1QnPe;";
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connString);
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                DataTable dt = new DataTable();
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd)) 
+                {
+                    adapter.Fill(dt);
+                }
+
+                return dt;
+            }
+        }
+        public void ExecuteNonQuery(string query)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
